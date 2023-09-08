d2542088878108f4b923e47f2162b40c92168b3a
TamaraChavez <tamara33chavez@gmail.com>
2023-09-08 11:32:16 -0600

    creación de metodos para lectura, guardar, y validar xmol en capa negocios

--- src/datos/XML.java
+++ src/datos/XML.java
@@ -1,11 +1,62 @@
 
 package datos;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 
+
+//leer documento xml
 public class XML {
-    public void LeerXML(String ruta)
+    public Document LeerXML(String strRuta)
     {
-
-        System.out.println("hola que tal");     
-     
+        try
+        {
+            DocumentBuilderFactory factory= DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document documento = builder.parse(new File(strRuta));
+          return documento; 
+        }catch (Exception ex)
+        {
+            return null; 
+        }      
     }
+    
+    //guardar documento xml
+    public void GuardarXml(String strRuta, Document documento)
+    {
+         try
+         {
+             TransformerFactory transformerFactory = TransformerFactory.newInstance();
+             Transformer transformer = transformerFactory.newTransformer();
+             DOMSource source = new DOMSource(documento);
+             StreamResult result= new StreamResult(new File(strRuta));
+             
+         }catch(Exception ex)
+         {
+           ex.printStackTrace(); 
+         }
+    }
+    
+    //validar si existe el xml
+    public boolean ValidarXml(String strRuta)        
+    {
+         try
+         {
+             File archivo = new File(strRuta);
+             return archivo.exists();
+         }catch (Exception ex)
+         {
+             ex.printStackTrace();
+             return false;
+         }
+    }
+    
+    
 }
--- src/negocio/Tarea1TamaraMariaJose.java
+++ src/negocio/Tarea1TamaraMariaJose.java
@@ -10,9 +10,6 @@ package negocio;
  */
 public class Tarea1TamaraMariaJose {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
